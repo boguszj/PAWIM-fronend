4332da8d6ce64fe997d9eaff8aa0d05ffbce4ad5
Jakub Bogusz <jbogusz@MacBook-Pro-Jakub.local>
2020-10-26 20:50:33 +0100

    Fix spinner

--- static/js/index/logic/spinner.mjs
+++ static/js/index/logic/spinner.mjs
@@ -2,4 +2,9 @@ import { MODAL_SELECTOR } from "../constants/index-constants.mjs"
 
 export const showSpinner = () => $(MODAL_SELECTOR).modal('show');
 
-export const hideSpinner = () => $(MODAL_SELECTOR).modal('hide');
+export const hideSpinner = () => {
+    $(MODAL_SELECTOR).on('shown.bs.modal', () => {
+        $(MODAL_SELECTOR).modal('hide');
+        $(MODAL_SELECTOR).on('shown', () => true);
+    })
+}
